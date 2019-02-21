ae12e1219858bec3eb483a9230859664341b6a09
david.betancur <david.betancur@pragma.com.co>
2019-02-21 09:42:44 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,91 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Header from './components/Header';
+import Formulario from './components/Formulario';
+import Axios from 'axios';
+import Resultado from './components/Resultado';
 
 class App extends Component {
+
+  state = {
+    monedas: [],
+    cotizacion: {},
+    monedaCotizada: '',
+    cargando: false
+  }
+
+  async componentDidMount() {
+    this.obtenerMonedas();
+  }
+
+  obtenerMonedas = async () => {
+    const url = `https://api.coinmarketcap.com/v2/ticker/`;
+
+    Axios.get(url).then( respuesta => {
+      this.setState({
+        monedas: respuesta.data.data
+      })
+    }).catch(error => {
+      console.log(error);
+    })
+  }
+
+  obternerValoresCrypto = async (monedas) => {
+    const {moneda, crypto} = monedas;
+    const url = `https://api.coinmarketcap.com/v2/ticker/${crypto}/?convert=${moneda}`;
+
+    await Axios.get(url)
+          .then(respuesta => {
+            this.setState({
+              cargando: true
+            })
+           setTimeout(()=>{
+            this.setState({
+              cotizacion:respuesta.data.data,
+              monedaCotizada:moneda,
+              cargando: false
+            })
+           }, 3000)
+          })
+  
+  }
+
   render() {
+
+    const cargando = this.state.cargando;
+
+    let resultado;
+
+    if(cargando) {
+      resultado = <div className="sk-cube-grid">
+      <div className="sk-cube sk-cube1"></div>
+      <div className="sk-cube sk-cube2"></div>
+      <div className="sk-cube sk-cube3"></div>
+      <div className="sk-cube sk-cube4"></div>
+      <div className="sk-cube sk-cube5"></div>
+      <div className="sk-cube sk-cube6"></div>
+      <div className="sk-cube sk-cube7"></div>
+      <div className="sk-cube sk-cube8"></div>
+      <div className="sk-cube sk-cube9"></div>
+    </div>
+    } else {
+      resultado =  <Resultado
+      cotizacion={this.state.cotizacion}
+      monedaCotizada = {this.state.monedaCotizada}
+    />
+    }
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="container">
+       <Header titulo = 'Cotiza Criptomonedas al instante' />
+       <div className="row justify-content-center">
+        <div className="col-md-6 bg-light pb-4 contenido-principal">
+          <Formulario
+            monedas = {this.state.monedas}
+            obternerValoresCrypto = {this.obternerValoresCrypto}
+          />
+            {resultado}
+        </div>
+       </div>
       </div>
     );
   }
---
+++ src/components/Formulario.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react';
+import OptionSelect from './OptionSelect';
+
+class Formulario extends Component {
+
+    monedaRef = React.createRef();
+    criptoRef = React.createRef();
+
+    cotizarMonedas = (e) => {
+        e.preventDefault();
+
+        const cotizacion = {
+            moneda: this.monedaRef.current.value,
+            crypto: this.criptoRef.current.value
+        }
+
+        this.props.obternerValoresCrypto(cotizacion);
+
+        // console.log(cotizacion);
+    } 
+
+    render() {
+        return (
+            <form onSubmit={this.cotizarMonedas}>
+                
+                <div className="form-group">
+                    <label>Moneda:</label>
+                    <select ref={this.monedaRef} className="form-control">
+                        <option value="" disabled defaultValue>Elige tu moneda</option>
+                        <option value="USD">Dolar estadounidense</option>
+                        <option value="MXN">Peso Mexicano</option>
+                        <option value="COP">Peso Colombiano</option>
+                        <option value="GBP">Libras</option>
+                        <option value="EUR">Euros</option>
+                    </select>
+                </div>
+
+                <div className="form-group">
+                    <label>Criptomoneda</label>
+                    <select ref={this.criptoRef} className="form-control">
+                        {Object.keys(this.props.monedas).map(key=>(
+                            <OptionSelect 
+                                key={key}
+                                moneda={this.props.monedas[key]}
+                            />
+                        ))}
+                    </select>
+                </div>
+                
+                <div className="form-group">
+                    <input type="submit" className="btn btn-primary" value="Cotizar"/>
+                </div>
+            </form>
+        );
+    }
+}
+
+export default Formulario;
